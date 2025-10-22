98b405541f900c834ef489c55c552e254ecdc502
behzad 1k <bhzd1k@gmail.com>
2025-10-22 19:12:49 +0330

    docker

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: '/hair-color-changer',
+  assetPrefix: '/hair-color-changer',
   experimental: {
     optimizePackageImports: [] // Disable if present
   }};
