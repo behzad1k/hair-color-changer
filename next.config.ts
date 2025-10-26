7fd58de0c1043fffe49e7caeea977c99311a7bc8
behzad 1k <bhzd1k@gmail.com>
2025-10-26 11:27:12 +0330

    env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/hair-color-changer',
   assetPrefix: '/hair-color-changer',
   experimental: {
     optimizePackageImports: [] // Disable if present
